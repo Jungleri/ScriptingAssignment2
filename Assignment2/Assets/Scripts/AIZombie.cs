abc3861f0b49111735f7aff82b25be43d28f3c9d
Jungleri <thomasjdickinson@hotmail.com>
2017-11-24 12:47:11 +0000

    UPD Starting runtime navmesh generation

---
+++ Assignment2/Assets/Scripts/AIZombie.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AIZombie : MonoBehaviour
+{
+    [SerializeField] Rigidbody[] ragdollBones;
+
+
+	void Start ()
+    {
+		foreach (Rigidbody rb in ragdollBones)
+        {
+            rb.isKinematic = true;
+        }
+	}
+	
+
+	void Update ()
+    {
+		
+	}
+}
