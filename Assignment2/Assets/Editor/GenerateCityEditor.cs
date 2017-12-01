8bb0a264958fef1b4736edfdb25341feb1f171df
Jungleri <thomasjdickinson@hotmail.com>
2017-12-01 11:07:24 +0000

    UPD Adding in city generation.

---
+++ Assignment2/Assets/Editor/GenerateCityEditor.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(CityGenerator))]
+public class GenerateCityEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        CityGenerator cityGenScript = (CityGenerator)target;
+        if (GUILayout.Button("Build City"))
+        {
+            cityGenScript.GenerateCity();
+        }
+
+        if (cityGenScript.autoUpdate)
+        {
+            cityGenScript.GenerateCity();
+        }
+    }
+}
